022b269643b5385c95be8215fb4bebe9ad89bc58
Ken Levasseur <klevasseuriv@gmail.com>
2021-11-20 16:03:15 -0500

    rework

--- tripad/_shared.py
+++
@@ -1,7 +0,0 @@
-"""Shared data and logic"""
-from kivy.uix.tabbedpanel import TabbedPanelItem
-
-
-class TripadItem(TabbedPanelItem):
-    # in case we want to add some shared functionality later
-    pass
--- tripad/kasacontrolitem.py
+++
@@ -1,6 +0,0 @@
-from tripad._shared import TripadItem
-
-
-class KasaControlItem(TripadItem):
-    def testbutton(self):
-        print("other words")
